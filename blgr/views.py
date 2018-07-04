1bcb328ed49670199c6f8a29e74d72771e72a7b1
genegl <yevglu@live.com>
2018-07-04 16:07:45 +0000

    Last couple classes to the /views.py/

--- blgr/views.py
+++ blgr/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from blgr.models import Post, Comment
 from blgr.forms import PostForm, CommentForm
+from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (TemplateView, ListView,
-			DetailView, CreateView)
+			DetailView, CreateView, UpdateView,
+			DeleteView)
 # Create your views here.
 
 class AboutView(TemplateView):
@@ -13,15 +15,34 @@ class AboutView(TemplateView):
 class PostListView(ListView):
 	model = Post
 
-	def get_queryset(self):
-		return Post.objects.filter(published_date__lte = timezone.now.order_by('-published_date'))	# __lte - less then
+	def get_queryset(self):		# we getting from database only filtered data
+		return Post.objects.filter(published_date__lte = timezone.now()).order_by('-published_date')	# __lte - less then
 																# or equal to. "-" desh before for descending order
 
-class PostDetailView():
+class PostDetailView(DetailView):
 	model = Post
 
-class CreatePostView(LoginRequiredMixin,CreateView):
+class CreatePostView(LoginRequiredMixin, CreateView):
 	login_url = '/login/'
 	redirect_field_name = 'blgr/post_detail.html'
 	form_class = PostForm
 	model = Post
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+	login_url = '/login/'
+	redirect_field_name = 'blgr/post_detail.html'
+	form_class = PostForm
+	model = Post
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+	model = Post
+	success_url = reverse_lazy('post_list')
+
+class DraftListView(LoginRequiredMixin, ListView):
+	login_url = '/login/'
+	redirect_field_name = 'blgr/post_list.html'
+	model = Post
+
+	def get_queryset(self):
+		return Post.objects.filter(published_date__isnull=True).order_by('created_date')		# __isnull, it is draft, so
+														# there is no pub_date
