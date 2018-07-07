6312a2b80619580165837fb4b6762da723ae186d
genegl <yevglu@live.com>
2018-07-07 04:31:29 +0000

    Added comments section to the /views.py/

--- blgr/views.py
+++ blgr/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from blgr.models import Post, Comment
 from blgr.forms import PostForm, CommentForm
 from django.urls import reverse_lazy
@@ -46,3 +47,42 @@ class DraftListView(LoginRequiredMixin, ListView):
 	def get_queryset(self):
 		return Post.objects.filter(published_date__isnull=True).order_by('created_date')		# __isnull, it is draft, so
 														# there is no pub_date
+
+
+
+
+###########################################		This is Comments related section
+###########################################
+
+@login_required
+def post_publish(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	post.publish
+	return redirect('post_detail', pk=pk)
+
+@login_required		# this is a decorator from django, the view below is requiret login
+def add_comment_to_post(request, pk):		# gettin the primary key for the post and request as parameters
+	post = get_object_or_404(Post, pk = pk)
+	if request.method == 'POST':
+		form = CommentForm(request.POST)
+		if form.is_valid():
+			comment = form.save(commit = False)		# we get data but not save to the DB, yet
+			comment.post = post
+			comment.save()							# now, we saved everything to the DB
+			return redirect('post_detail', pk=post.pk)
+	else:
+		form = CommentForm()	# here we just showing the form
+	return render(request, 'blgr/comment_form.html', {'form':form})
+
+@login_required
+def comment_approve(request, pk):
+	comment = get_object_or_404(Comment, pk=pk)
+	comment.approve()		# the Comment and approve() are located in the /models.py/
+	return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+	comment = get_object_or_404(Comment, pk=pk)
+	post_pk = comment.post.pk 		# temporary saving post's primary key
+	comment.delete()
+	return redirect('post_detail', pk = post_pk)	# here we used the saved primary key
